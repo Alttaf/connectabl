ab67e5c966531cad914944109839a0c66a2ed7d6
Alttaf <alttaf.hussain@gmail.com>
2016-05-31 01:45:30 +0100

    begin cleanup

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -15,9 +15,7 @@ function onConnect(socket) {
   socket.on('info', data => {
     socket.log(JSON.stringify(data, null, 2));
   });
-
-  // Insert sockets below
-  require('../api/thing/thing.socket').register(socket);
+  
 
 }
 
