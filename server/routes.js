8bcbf860a7dbb516b97787273a97e1ccc48b17f3
Alttaf <alttaf.hussain@gmail.com>
2016-06-01 03:36:59 +0100

    cache controll

--- server/routes.js
+++ server/routes.js
@@ -6,12 +6,15 @@
 
 import errors from './components/errors';
 import path from 'path';
+import cacheResponseDirective from 'express-cache-response-directive';
 
 export default function(app) {
   // Insert routes below
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth').default);
+
+  app.use(cacheResponseDirective());
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
